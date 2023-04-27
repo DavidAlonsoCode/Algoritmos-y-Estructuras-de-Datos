bf69961dd1d24d6cc871d257d9a1a7cedc0ed6d5
David <dave3000ea@gmail.com>
2023-04-27 17:20:21 -0300

    Update Hola.cpp

--- 00-CppHelloWorld/Hola.cpp
+++ 00-CppHelloWorld/Hola.cpp
@@ -1,7 +1,13 @@
+/* Hello World
+ * Primer programa, hello world
+ * Autor: David Emanuel Alonso
+ * 27-4-2023
+ */
+
 #include<iostream>
 
 int main(){
 
-    std::cout<<"Hola todos";
+    std::cout<<"Hello world";
     
 }
